6cdffc7d0387bd04419e115dcfbb6b432b4fab34
Pietro Albini <pietro@pietroalbini.io>
2015-06-27 11:38:12 +0200

    Added the skeleton of the docs

---
+++ docs/conf.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+    botogram documentation"s config file
+
+    Copyright (c) 2015 Pietro Albini
+    Released under the MIT license
+"""
+import sys
+import os
+import shlex
+
+extensions = []
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+
+project = "botogram"
+copyright = "2015, Pietro Albini"
+author = "Pietro Albini"
+
+version = "0.1"
+release = "0.1"
+
+language = None
+
+exclude_patterns = ["_build"]
+
+pygments_style = "sphinx"
+
+todo_include_todos = False
+
+
+## HTML output
+
+html_theme = "botogram"
+html_theme_path = ["_themes"]
+html_static_path = ["_static"]
+
+html_sidebars = {
+    "**": ["links.html"]
+}
+#html_additional_pages = {}
+#html_domain_indices = True
+#html_use_index = True
+#html_split_index = False
+#html_show_sourcelink = True
+#html_show_sphinx = True
+#html_show_copyright = True
+#html_search_language = "en"
+#html_search_options = {"type": "default"}
+#html_search_scorer = "scorer.js"
+
+htmlhelp_basename = "botogramdoc"
+
+
+## Latex output
+
+latex_elements = {
+#   "papersize": "letterpaper",
+#   "pointsize": "10pt",
+#   "preamble": "",
+#   "figure_align": "htbp",
+}
+
+latex_documents = [
+  (master_doc, "botogram.tex", "botogram Documentation",
+   "Pietro Albini", "manual"),
+]
+
+#latex_logo = None
+#latex_use_parts = False
+#latex_show_pagerefs = False
+#latex_show_urls = False
+#latex_appendices = []
+#latex_domain_indices = True
+
+
+## Manpages output
+
+man_pages = [
+    (master_doc, "botogram", "botogram Documentation",
+     [author], 1)
+]
+#man_show_urls = False
+
+
+## Texinfo output
+
+texinfo_documents = [
+  (master_doc, "botogram", "botogram Documentation",
+   author, "botogram", "One line description of project.",
+   "Miscellaneous"),
+]
+#texinfo_appendices = []
+#texinfo_domain_indices = True
+#texinfo_show_urls = "footnote"
+#texinfo_no_detailmenu = False
---
+++ docs/index.rst
@@ -0,0 +1,34 @@
+.. Copyright (c) 2015 Pietro Albini
+   Released under the MIT licende
+
+~~~~~~~~~~~~~~~~~~~~~~
+botogram documentation
+~~~~~~~~~~~~~~~~~~~~~~
+
+*A microframework for Telegram bots.*
+
+botogram is a MIT-licensed microframework, which aims to simplify the creation
+of `Telegram bots`_. It offers a concise, simple API, which allows you to spend
+all your creativity in the bot, without worrying about anything else.
+
+It also provides a robust, fully scalable bot runner process, which will be
+able to process fastly high workloads. And as if this isn't enough, it has
+builtin support for commands, with an automatically-generated ``/help``
+command.
+
+::
+
+   import botogram
+   bot = botogram.create("YOUR-API-KEY")
+
+   @bot.command("hello")
+   def hello_command(chat, message, args):
+      """Say hello to the world!"""
+      chat.send("Hello world")
+
+   bot.run()
+
+.. toctree::
+   :maxdepth: 2
+
+.. _Telegram bots: https://core.telegram.org/bots
