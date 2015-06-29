563762ed7f2fbcdf1e0aa0e7d3c991f75bc7c56a
Pietro Albini <pietro@pietroalbini.io>
2015-06-29 09:01:08 +0200

    Trimmed down the pygments style and styled highlighted lines

--- docs/_ext/botogramext.py
+++ docs/_ext/botogramext.py
@@ -13,6 +13,7 @@ OPERATORS = "#444"
 class BotogramStyle(style.Style):
     """Pygments style for the botogram documentation"""
     background_color = "#fff"
+    highlight_color = "#f3f3f3"
     default_style = ""
 
     styles = {
@@ -21,15 +22,8 @@ class BotogramStyle(style.Style):
         token.Other:                     NAMES,                # class x
 
         token.Comment:                   COMMENTS, # c
-        token.Comment.Preproc:           COMMENTS,       # cp
 
         token.Keyword:                   KEYWORD, # k
-        token.Keyword.Constant:          KEYWORD, # kc
-        token.Keyword.Declaration:       KEYWORD, # kd
-        token.Keyword.Namespace:         KEYWORD, # kn
-        token.Keyword.Pseudo:            KEYWORD, # kp
-        token.Keyword.Reserved:          KEYWORD, # kr
-        token.Keyword.Type:              KEYWORD, # kt
 
         token.Operator:                  OPERATORS, # o
         token.Operator.Word:             KEYWORD, # ow
@@ -37,42 +31,16 @@ class BotogramStyle(style.Style):
         token.Punctuation:               NAMES, # p
 
         token.Name:                      NAMES, # n
-        token.Name.Attribute:            NAMES, # na
-        token.Name.Builtin:              NAMES, # nb
-        token.Name.Builtin.Pseudo:       NAMES, # bp
-        token.Name.Class:                NAMES, # nc
-        token.Name.Constant:             NAMES, # no
         token.Name.Decorator:            "bold "+OPERATORS, # nd
         token.Name.Entity:               "#ce5c00", # ni
-        token.Name.Exception:            NAMES, # ne
-        token.Name.Function:             NAMES, # nf
-        token.Name.Property:             NAMES, # py
-        token.Name.Label:                NAMES, # nl
-        token.Name.Namespace:            NAMES, # nn
-        token.Name.Other:                NAMES, # nx
         token.Name.Tag:                  KEYWORD, # nt
-        token.Name.Variable:             NAMES, # nv
-        token.Name.Variable.Class:       NAMES, # vc
-        token.Name.Variable.Global:      NAMES, # vg
-        token.Name.Variable.Instance:    NAMES, # vi
 
         token.Number:                    NUMBERS, # m
 
         token.Literal:                   NAMES, # l
-        token.Literal.Date:              NAMES, # ld
 
         token.String:                    STRING, # s
-        token.String.Backtick:           STRING, # sb
-        token.String.Char:               STRING, # sc
         token.String.Doc:                COMMENTS, # sd
-        token.String.Double:             STRING, # s2
-        token.String.Escape:             STRING, # se
-        token.String.Heredoc:            STRING, # sh
-        token.String.Interpol:           STRING, # si
-        token.String.Other:              STRING, # sx
-        token.String.Regex:              STRING, # sr
-        token.String.Single:             STRING, # s1
-        token.String.Symbol:             STRING, # ss
 
         token.Generic:                   NAMES,        # g
         token.Generic.Deleted:           "#a40000",        # gd
