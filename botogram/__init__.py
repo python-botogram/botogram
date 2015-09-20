b179998693e079c719215d9201ee0406558768af
Pietro Albini <pietro@pietroalbini.io>
2015-09-20 23:39:37 +0200

    Added an environment variable to simulate Windows on Linux and OSX.

--- botogram/__init__.py
+++ botogram/__init__.py
@@ -13,3 +13,14 @@ from .components import Component
 from .decorators import pass_bot, pass_shared, help_message_for
 from .objects import *
 from .utils import usernames_in
+
+
+# This code will simulate the Windows' multiprocessing behavior if the
+# BOTOGRAM_SIMULATE_WINDOWS environment variable is set
+import os
+import multiprocessing
+
+if "BOTOGRAM_SIMULATE_WINDOWS" in os.environ:
+    multiprocessing.set_start_method("spawn", force=True)
+
+del os, multiprocessing
