python-botogram/botogram

commit 350a78aae4bf8327784b91ffa9091021e8c30721

Fix a bunch of errors in the Windows support
Before this commit, when you use any hook except the command one on
Windows, your bot crashes because pickle doesn't know how to serialize
the hook. That was because botogram changed the actual function of the
hook, and pickle don't like that.

This commit refactors almost all the hooks creation logic, moving away
from modifying the user's function on the fly.

botogram/hooks.py
@@ -1,0 +1,183 @@
+"""
+    botogram.hooks
+    Logic for all the hooks
+
+    Copyright (c) 2016 Pietro Albini <pietro@pietroalbini.io>
+    Released under the MIT license
+"""
+
+import re
+
+
+class Hook:
+    """Base class for all the hooks"""
+
+    _only_texts = False
+    _botogram_hook = True
+
+    def __init__(self, func, component, args=None):
+        prefix = ""
+        if component.component_name:
+            prefix = component.component_name+"::"
+
+        self.func = func
+        self.name = prefix+func.__name__
+        self.component = component
+
+        self._args = args
+        self._after_init(args)
+
+    def __reduce__(self):
+        return rebuild, (self.__class__, self.func, self.component, self._args)
+
+    def __repr__(self):
+        return "<"+self.__class__.__name__+" \""+self.name+"\">"
+
+    def _after_init(self, args):
+        """Prepare the object"""
+        pass
+
+    def call(self, bot, update):
+        """Call the hook"""
+        if self._only_texts and update.message is None:
+            return
+        return self._call(bot, update)
+
+    def _call(self, bot, update):
+        """*Actually* call the hook"""
+        message = update.message
+        return bot._call(self.func, chat=message.chat, message=message)
+
+
+def rebuild(cls, func, component, args):
+    hook = cls(func, component, args)
+    return hook
+
+
+class BeforeProcessingHook(Hook):
+    """Underlying hook for @bot.process_message"""
+    pass
+
+
+class ProcessMessageHook(Hook):
+    """Underlying hook for @bot.process_message"""
+    pass
+
+
+class SharedMemoryInitializerHook(Hook):
+    """Underlying hook for @bot.init_shared_memory"""
+    pass
+
+
+class NoCommandsHook(Hook):
+    """Underlying hook for an internal event"""
+    pass
+
+
+class MessageEqualsHook(Hook):
+    """Underlying hook for @bot.message_equals"""
+
+    _only_texts = True
+
+    def _after_init(self, args):
+        if args["ignore_case"]:
+            self._string = args["string"].lower()
+        else:
+            self._string = args["string"]
+
+    def _prepare(self, update):
+        message = update.message
+        text = message.text
+        if self._args["ignore_case"]:
+            text = text.lower()
+
+        return message, text
+
+    def _call(self, bot, update):
+        message, text = self._prepare(update)
+
+        if text != self._string:
+            return
+        return bot._call(self.func, chat=message.chat, message=message)
+
+
+class MessageContainsHook(MessageEqualsHook):
+    """Underlying hook for @bot.message_contains"""
+
+    _only_texts = True
+
+    def _call(self, bot, update):
+        message, text = self._prepare(update)
+        splitted = text.split(" ")
+
+        res = []
+        for one in splitted:
+            if one != self._string:
+                continue
+
+            result = bot._call(self.func, chat=message.chat, message=message)
+            res.append(result)
+            if not self._args["multiple"]:
+                break
+
+        return len(res) > 0
+
+
+class MessageMatchesHook(Hook):
+    """Underlying hook for @bot.message_matches"""
+
+    _only_texts = True
+
+    def _after_init(self, args):
+        self._regex = re.compile(args["regex"], flags=args["flags"])
+
+    def _call(self, bot, update):
+        message = update.message
+        results = self._regex.finditer(message.text)
+
+        found = False
+        for result in results:
+            found = True
+
+            bot._call(self.func, chat=message.chat, message=message,
+                      matches=result.groups())
+            if not self._args["multiple"]:
+                break
+
+        return found
+
+
+class CommandHook(Hook):
+    """Underlying hook for @bot.command"""
+
+    _only_texts = True
+
+    def _after_init(self, args):
+        # Check if the command name is valid
+        if not re.match(r'^[a-zA-Z0-9_]+$', args["name"]):
+            raise ValueError("Invalid command name: %s" % args["name"])
+
+        # This regex will match all commands pointed to this bot
+        self._regex = re.compile(r'^\/'+args["name"]+r'(@[a-zA-Z0-9_]+)?'
+                                 r'( .*)?$')
+
+    def _call(self, bot, update):
+        message = update.message
+
+        # Must be the correct command for the correct bot
+        match = self._regex.match(message.text)
+        if not match:
+            return
+        if match.group(1) and match.group(1) != "@"+bot.itself.username:
+            return
+
+        args = message.text.split(" ")[1:]
+        bot._call(self.func, chat=message.chat, message=message, args=args)
+        return True
+
+
+class TimerHook(Hook):
+    """Underlying hook for a timer"""
+
+    def call(self, bot):
+        return bot._call(self.func)

botogram/components.py
@@ -2,15 +2,15 @@
     botogram.components
     Definition of the components system
 
-    Copyright (c) 2015 Pietro Albini
+    Copyright (c) 2015-2016 Pietro Albini
     Released under the MIT license
 """
 
-import re
 import uuid
 
 from . import utils
 from . import tasks
+from . import hooks
 
 
 class Component:
@@ -47,34 +47,27 @@ class Component:
         if not callable(func):
             raise ValueError("A before processing hook must be callable")
 
-        self.__before_processors.append(func)
+        hook = hooks.BeforeProcessingHook(func, self)
+        self.__before_processors.append(hook)
 
     def add_process_message_hook(self, func):
         """Add a message processor hook"""
         if not callable(func):
             raise ValueError("A message processor must be callable")
 
-        self.__processors.append(func)
+        hook = hooks.ProcessMessageHook(func, self)
+        self.__processors.append(hook)
 
     def add_message_equals_hook(self, string, func, ignore_case=True):
         """Add a message equals hook"""
         if not callable(func):
             raise ValueError("A message equals hook must be callable")
 
-        if ignore_case:
-            string = string.lower()
-
-        @utils.wraps(func)
-        def wrapped(bot, chat, message):
-            text = message.text
-            if ignore_case:
-                text = text.lower()
-
-            if text != string:
-                return
-            return bot._call(func, chat=chat, message=message)
-
-        self.add_process_message_hook(wrapped)
+        hook = hooks.MessageEqualsHook(func, self, {
+            "ignore_case": ignore_case,
+            "string": string,
+        })
+        self.__processors.append(hook)
 
     def add_message_contains_hook(self, string, func, ignore_case=True,
                                   multiple=False):
@@ -82,40 +75,24 @@ class Component:
         if not callable(func):
             raise ValueError("A message contains hook must be callable")
 
-        regex = r'\b('+string+r')\b'
-        flags = re.IGNORECASE if ignore_case else 0
-
-        @utils.wraps(func)
-        def wrapped(bot, chat, message, matches):
-            return bot._call(func, chat=chat, message=message)
-
-        self.add_message_matches_hook(regex, wrapped, flags, multiple)
+        hook = hooks.MessageContainsHook(func, self, {
+            "ignore_case": ignore_case,
+            "multiple": multiple,
+            "string": string,
+        })
+        self.__processors.append(hook)
 
     def add_message_matches_hook(self, regex, func, flags=0, multiple=False):
         """Apply a message matches hook"""
         if not callable(func):
             raise ValueError("A message matches hook must be callable")
 
-        @utils.wraps(func)
-        def processor(bot, chat, message):
-            if message.text is None:
-                return
-
-            compiled = re.compile(regex, flags=flags)
-            results = compiled.finditer(message.text)
-
-            found = False
-            for result in results:
-                found = True
-
-                bot._call(func, chat=chat, message=message,
-                          matches=result.groups())
-                if not multiple:
-                    break
-
-            return found
-
-        self.__processors.append(processor)
+        hook = hooks.MessageMatchesHook(func, self, {
+            "flags": flags,
+            "multiple": multiple,
+            "regex": regex,
+        })
+        self.__processors.append(hook)
 
     def add_command(self, name, func, _from_main=False):
         """Register a new command"""
@@ -130,15 +107,18 @@ class Component:
             utils.warn(go_back, "Command names shouldn't be prefixed with a "
                        "slash. It's done automatically.")
 
-        self.__commands[name] = self.__wrap_function(func)
+        hook = hooks.CommandHook(func, self, {
+            "name": name,
+        })
+        self.__commands[name] = hook
 
     def add_timer(self, interval, func):
         """Register a new timer"""
         if not callable(func):
             raise ValueError("A timer must be callable")
 
-        wrapped = self.__wrap_function(func)
-        job = tasks.TimerTask(interval, wrapped)
+        hook = hooks.TimerHook(func, self)
+        job = tasks.TimerTask(interval, hook)
 
         self.__timers.append(job)
 
@@ -147,24 +127,25 @@ class Component:
         if not callable(func):
             raise ValueError("A shared memory initializer must be callable")
 
-        self.__shared_inits.append(self.__wrap_function(func))
+        hook = hooks.SharedMemoryInitializerHook(func, self)
+        self.__shared_inits.append(hook)
 
     def _add_no_commands_hook(self, func):
         """Register an hook which will be executed when no commands matches"""
         if not callable(func):
             raise ValueError("A no commands hook must be callable")
 
-        self.__no_commands.append(func)
+        hook = hooks.NoCommandsHook(func, self)
+        self.__no_commands.append(hook)
 
     def _get_hooks_chain(self):
         """Get the full hooks chain for this component"""
-        chain = [
-            self.__before_processors,
-            self.__generate_commands_processors(),
-            self.__no_commands,
-            self.__processors,
+        return [
+            self.__before_processors[:],
+            [self.__commands[name] for name in sorted(self.__commands.keys())],
+            self.__no_commands[:],
+            self.__processors[:],
         ]
-        return [[self.__wrap_function(f) for f in c] for c in chain]
 
     def _get_commands(self):
         """Get all the commands this component implements"""
@@ -177,41 +158,3 @@ class Component:
     def _get_timers(self):
         """Get a list of all the timers"""
         return self.__timers
-
-    def __generate_commands_processors(self):
-        """Generate a list of commands processors"""
-        def base(name, func):
-            @utils.wraps(func)
-            def __(bot, chat, message):
-                # Commands must have a message
-                if message.text is None:
-                    return
-
-                # Must be this command
-                match = bot._commands_re.match(message.text)
-                if not match or match.group(1) != name:
-                    return
-
-                args = message.text.split(" ")[1:]
-                bot._call(func, chat=chat, message=message, args=args)
-                return True
-            return __
-
-        return [base(name, func) for name, func in self.__commands.items()]
-
-    def __wrap_function(self, func):
-        """Wrap a function, adding to it component-specific things"""
-        # This allows us to wrap methods
-        real_func = func
-        if hasattr(func, "__func__"):
-            func = func.__func__
-
-        if not hasattr(func, "botogram"):
-            func.botogram = utils.HookDetails(func)
-
-        prefix = self.component_name+"::" if self.component_name else ""
-
-        func.botogram.name = prefix+func.__name__
-        func.botogram.component = self
-
-        return real_func

botogram/decorators.py
@@ -2,7 +2,7 @@
     botogram.decorators
     Utility decorators used by the bot creators
 
-    Copyright (c) 2015 Pietro Albini <pietro@pietroalbini.io>
+    Copyright (c) 2015-2016 Pietro Albini <pietro@pietroalbini.io>
     Released under the MIT license
 """
 
@@ -27,9 +27,6 @@ def help_message_for(func):
     """The return of the decorated function will be the help message of the
     function provided as an argument."""
     def decorator(help_func):
-        if not hasattr(func, "botogram"):
-            func.botogram = utils.HookDetails(func)
-
-        func.botogram.help_message = help_func
+        func._botogram_help_message = help_func
         return help_func
     return decorator

botogram/frozenbot.py
@@ -2,7 +2,7 @@
     botogram.frozenbot
     A frozen version of the real bot
 
-    Copyright (c) 2015 Pietro Albini <pietro@pietroalbini.io>
+    Copyright (c) 2015-2016 Pietro Albini <pietro@pietroalbini.io>
     Released under the MIT license
 """
 
@@ -165,16 +165,11 @@ class FrozenBot:
 
         for hook in self._chain:
             # Get the correct name of the hook
-            try:
-                name = hook.botogram.name
-            except AttributeError:
-                name = hook.__name__
-
+            name = hook.name
             self.logger.debug("Processing update #%s with the %s hook..." %
                               (update.update_id, name))
 
-            result = self._call(hook, chat=update.message.chat,
-                                message=update.message)
+            result = hook.call(self, update)
             if result is True:
                 self.logger.debug("Update #%s was just processed by the %s "
                                   "hook." % (update.update_id, name))

botogram/runner/jobs.py
@@ -2,7 +2,7 @@
     botogram.runner.jobs
     Definition of the jobs runner workers' can execute
 
-    Copyright (c) 2015 Pietro Albini <pietro@pietroalbini.io>
+    Copyright (c) 2015-2016 Pietro Albini <pietro@pietroalbini.io>
     Released under the MIT license
 """
 
@@ -99,6 +99,6 @@ def process_update(bot, metadata):
 def process_task(bot, metadata):
     """Process a generic task"""
     task = metadata["task"]
-    bot.logger.debug("Processing task %s..." % task.func.botogram.name)
+    bot.logger.debug("Processing task %s..." % task.hook.name)
 
     task.process(bot)

botogram/tasks.py
@@ -2,7 +2,7 @@
     botogram.tasks
     Core implementation of tasks and timers
 
-    Copyright (c) 2015 Pietro Albini <pietro@pietroalbini.io>
+    Copyright (c) 2015-2016 Pietro Albini <pietro@pietroalbini.io>
     Released under the MIT license
 """
 
@@ -12,22 +12,24 @@ import time
 class BaseTask:
     """A basic task"""
 
-    def __init__(self, func):
-        self.func = func
+    def __init__(self, hook):
+        self.hook = hook
 
     def process(self, bot):
         """Process the task"""
-        return bot._call(self.func)
+        if hasattr(self.hook, "call"):
+            return self.hook.call(bot)
+        return bot._call(self.hook)
 
 
 class TimerTask(BaseTask):
     """Representation of a single timer"""
 
-    def __init__(self, interval, func):
+    def __init__(self, interval, hook):
         self.interval = interval
         self.last_run = -interval
 
-        super(TimerTask, self).__init__(func)
+        super(TimerTask, self).__init__(hook)
 
     def now(self, current=None):
         """Check if the timer should be ran now"""

botogram/utils.py
@@ -2,7 +2,7 @@
     botogram.utils
     Utilities used by the rest of the code
 
-    Copyright (c) 2015 Pietro Albini <pietro@pietroalbini.io>
+    Copyright (c) 2015-2016 Pietro Albini <pietro@pietroalbini.io>
     Released under the MIT license
 """
 
@@ -94,13 +94,15 @@ def format_docstr(docstring):
 
 def docstring_of(func, bot=None):
     """Get the docstring of a function"""
-    # Custom docstring
-    if hasattr(func, "botogram") and func.botogram.help_message:
+    # Get the correct function from the hook
+    if hasattr(func, "_botogram_hook"):
+        func = func.func
+
+    if hasattr(func, "_botogram_help_message"):
         if bot is not None:
-            docstring = bot._call(func.botogram.help_message)
+            docstring = bot._call(func._botogram_help_message)
         else:
-            docstring = func.botogram.help_message()
-    # Standard default docstring
+            docstring = func._botogram_help_message()
     elif func.__doc__:
         docstring = func.__doc__
     # Put a default message
@@ -166,16 +168,3 @@ def configure_logger():
 
     # Don't reconfigure the logger, thanks
     _logger_configured = True
-
-
-class HookDetails:
-    """Container for some details of user-provided hooks"""
-
-    def __init__(self, func):
-        self._func = func
-        self.name = ""
-        self.component = None
-        self.help_message = None
-
-    def _default_help_message(self):
-        return format_docstr(self._func.__doc__)

commit ad2371709e136d40923db7e29b2b889b74b37085

Fix wrong handling of non-text messages
This happened only when there were commands registered, and a non-text
message was sent to the bot. Regression introduced in 350a78a.

botogram/hooks.py
@@ -39,7 +39,7 @@ class Hook:
 
     def call(self, bot, update):
         """Call the hook"""
-        if self._only_texts and update.message is None:
+        if self._only_texts and update.message.text is None:
             return
         return self._call(bot, update)
 