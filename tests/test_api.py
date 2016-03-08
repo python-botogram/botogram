683521de7b82a035b29d19e60a2a9bd99b37704e
Pietro Albini <pietro@pietroalbini.io>
2016-03-08 16:34:55 +0100

    Add a custom exception when a chat isn't available
    This commit adds an easy way to detect when you're sending messages to
    a chat you can't send messages to. This includes when the user blocked
    your bot, you didn't contact the user before, or your bot was kicked
    from the group.

--- tests/test_api.py
+++ tests/test_api.py
@@ -31,3 +31,76 @@ def test_api_call(api, mock_req):
     # And then try to call something which doesn't exist
     with pytest.raises(botogram.api.APIError):
         result = api.call("wrong")
+
+
+def test_unavailable_chats(api, mock_req):
+    # A bunch of mocked requests for the API
+    mock_req({
+        "sendMessage": {"ok": True, "result": {}},
+        "forwardMessage": {
+            "ok": False, "error_code": 123, "description": "test",
+        },
+        "sendPhoto": {
+            "ok": False, "error_code": 403, "description": "test",
+        },
+        "sendAudio": {
+            "ok": False, "error_code": 123, "description": "blocked test",
+        },
+        "sendDocument": {
+            "ok": False, "error_code": 403, "description": "blocked test",
+        },
+        "sendSticker": {
+            "ok": False, "error_code": 400, "description": "chat not found",
+        },
+        "sendVideo": {
+            "ok": False, "error_code": 403, "description": "I was kicked!",
+        },
+        "getMe": {
+            "ok": False, "error_code": 403, "description": "blocked test",
+        },
+    })
+
+    # Test a successiful request
+    api.call("sendMessage", {"chat_id": 123})
+
+    # Test a failed request with wrong error code and description
+    with pytest.raises(botogram.api.APIError) as e:
+        api.call("forwardMessage", {"chat_id": 123})
+    assert e.type != botogram.api.ChatUnavailableError
+
+    # Test a failed request with matching error code and wrong description
+    with pytest.raises(botogram.api.APIError) as e:
+        api.call("sendPhoto", {"chat_id": 123})
+    assert e.type != botogram.api.ChatUnavailableError
+
+    # Test a failed request with wrong error code and matching description
+    with pytest.raises(botogram.api.APIError) as e:
+        api.call("sendAudio", {"chat_id": 123})
+    assert e.type != botogram.api.ChatUnavailableError
+
+    # Test a failed request with matching error code and description for
+    # blocked users
+    with pytest.raises(botogram.api.ChatUnavailableError) as e:
+        api.call("sendDocument", {"chat_id": 123})
+    assert e.value.chat_id == 123
+    assert e.value.reason == "blocked"
+
+    # Test a failed request with matching error code and description for chats
+    # not found
+    with pytest.raises(botogram.api.ChatUnavailableError) as e:
+        api.call("sendSticker", {"chat_id": 123})
+    assert e.value.chat_id == 123
+    assert e.value.reason == "not_found"
+
+    # Test a failed request with matching error code and description for a bot
+    # kicked from the group
+    with pytest.raises(botogram.api.ChatUnavailableError) as e:
+        api.call("sendVideo", {"chat_id": 123})
+    assert e.value.chat_id == 123
+    assert e.value.reason == "kicked"
+
+    # Test a failed request with right error code and descriptio to the wrong
+    # method (not a method which sends things to users)
+    with pytest.raises(botogram.api.APIError) as e:
+        api.call("getMe", {"chat_id": 123})
+    assert e.type != botogram.api.ChatUnavailableError
