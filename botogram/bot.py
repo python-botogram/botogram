python-botogram/botogram

commit 99f24a0dc062ba6c127462e13a9ffd6f1e7fd73a

Refactored the hooks chain
 This new implementation freezes the chain when the bot is frozen, and
 supports multiple chains, without the need to reimplement the logic
 everytime you add a new chain.

botogram/bot.py
@@ -54,7 +54,7 @@ class Bot(frozenbot.FrozenBot):
         self._shared_memory = shared.SharedMemory()
 
         # Register bot's shared memory initializers
-        inits = self._main_component._get_memory_preparers()
+        inits = self._main_component._get_chains()["memory_preparers"][0]
         maincompid = self._main_component._component_id
         self._shared_memory.register_preparers_list(maincompid, inits)
 
@@ -162,12 +162,13 @@ class Bot(frozenbot.FrozenBot):
                 self._components.append(component)
 
             # Register initializers for the shared memory
+            chains = component._get_chains()
             compid = component._component_id
-            preparers = component._get_memory_preparers()
+            preparers = chains["memory_preparers"][0]
             self._shared_memory.register_preparers_list(compid, preparers)
 
             # Register tasks
-            self._scheduler.register_tasks_list(component._get_timers())
+            self._scheduler.register_tasks_list(chains["tasks"][0])
 
     def process(self, update):
         """Process an update object"""
@@ -184,12 +185,14 @@ class Bot(frozenbot.FrozenBot):
 
     def freeze(self):
         """Return a frozen instance of the bot"""
+        chains = components.merge_chains(self._main_component,
+                                         *self._components)
+
         return frozenbot.FrozenBot(self.api, self.about, self.owner,
                                    self.hide_commands, self.before_help,
                                    self.after_help, self.process_backlog,
                                    self.lang, self.itself, self._commands_re,
-                                   self._components+[self._main_component],
-                                   self._scheduler,
+                                   chains, self._scheduler,
                                    self._main_component._component_id,
                                    self._bot_id, self._shared_memory)
 

botogram/frozenbot.py
@@ -22,7 +22,7 @@ class FrozenBot:
 
     def __init__(self, api, about, owner, hide_commands, before_help,
                  after_help, process_backlog, lang, itself, commands_re,
-                 components, scheduler, main_component_id, bot_id,
+                 chains, scheduler, main_component_id, bot_id,
                  shared_memory):
         # This attribute should be added with the default setattr, because is
         # needed by the custom setattr
@@ -38,25 +38,11 @@ class FrozenBot:
         self.process_backlog = process_backlog
         self.lang = lang
         self._commands_re = commands_re
-        self._components = components
         self._main_component_id = main_component_id
         self._bot_id = bot_id
         self._shared_memory = shared_memory
         self._scheduler = scheduler
-
-        # Rebuild the hooks chain and commands list
-        self._commands = components[-1]._get_commands()
-        self._chain = []
-        chains = components[-1]._get_hooks_chain()
-        for component in reversed(components[:-1]):
-            self._commands.update(component._get_commands())
-
-            comp_chain = component._get_hooks_chain()
-            for i in range(len(chains)):
-                chains[i] += comp_chain[i]
-
-        for chain in chains:
-            self._chain += chain
+        self._chains = chains
 
         # Setup the logger
         self.logger = logbook.Logger('botogram bot')
@@ -75,7 +61,7 @@ class FrozenBot:
         args = (
             self.api, self.about, self.owner, self.hide_commands,
             self.before_help, self.after_help, self.process_backlog,
-            self.lang, self.itself, self._commands_re, self._components,
+            self.lang, self.itself, self._commands_re, self._chains,
             self._scheduler, self._main_component_id, self._bot_id,
             self._shared_memory,
         )
@@ -186,7 +172,7 @@ class FrozenBot:
 
         update.set_api(self.api)  # Be sure to use the correct API object
 
-        for hook in self._chain:
+        for hook in self._chains["messages"]:
             # Get the correct name of the hook
             name = hook.name
             self.logger.debug("Processing update #%s with the %s hook..." %

botogram/components.py
@@ -144,23 +144,49 @@ class Component:
         hook = hooks.NoCommandsHook(func, self)
         self.__no_commands.append(hook)
 
-    def _get_hooks_chain(self):
+    def _get_chains(self):
         """Get the full hooks chain for this component"""
-        return [
+        messages = [
             self.__before_processors[:],
             [self.__commands[name] for name in sorted(self.__commands.keys())],
             self.__no_commands[:],
             self.__processors[:],
         ]
+        return {
+            "messages": messages,
+            "memory_preparers": [self.__memory_preparers],
+            "tasks": [self.__timers],
+        }
 
     def _get_commands(self):
         """Get all the commands this component implements"""
         return self.__commands
 
-    def _get_memory_preparers(self):
-        """Get a list of all the shared memory initializers"""
-        return self.__memory_preparers
 
-    def _get_timers(self):
-        """Get a list of all the timers"""
-        return self.__timers
+def merge_chains(main, *components):
+    """Merge multiple chains returned by the components"""
+    merged = {}
+    components = [main]+list(reversed(components))
+
+    # First of all, merge all the subchains of the different components
+    # together -- This is a separate step so the order is preserved
+    for component in components:
+        macrochains = component._get_chains()
+        for kind, chains in macrochains.items():
+            if kind not in merged:
+                merged[kind] = []
+
+            for i, chain in enumerate(chains):
+                try:
+                    merged[kind][i] += chain
+                except IndexError:
+                    merged[kind].append(chain[:])
+
+    # Then merge all the subchains together
+    result = {}
+    for kind, chains in merged.items():
+        result[kind] = []
+        for chain in chains:
+            result[kind] += chain
+
+    return result

commit 60d74937d5ad4e328723c940b18887471b6572d0

Fix GH-37 -- Broken /help command
This commit fixes a regression introduced in commit 99f24a0, which
removed a private attribute of FrozenBot used by the default /help
command. This commit reimplements that in order to restore the command.

botogram/bot.py
@@ -188,11 +188,17 @@ class Bot(frozenbot.FrozenBot):
         chains = components.merge_chains(self._main_component,
                                          *self._components)
 
+        # Get the list of commands for the bot
+        commands = self._components[-1]._get_commands()
+        for component in reversed(self._components[:-1]):
+            commands.update(component._get_commands())
+        commands.update(self._main_component._get_commands())
+
         return frozenbot.FrozenBot(self.api, self.about, self.owner,
                                    self.hide_commands, self.before_help,
                                    self.after_help, self.process_backlog,
                                    self.lang, self.itself, self._commands_re,
-                                   chains, self._scheduler,
+                                   commands, chains, self._scheduler,
                                    self._main_component._component_id,
                                    self._bot_id, self._shared_memory)
 

botogram/frozenbot.py
@@ -22,7 +22,7 @@ class FrozenBot:
 
     def __init__(self, api, about, owner, hide_commands, before_help,
                  after_help, process_backlog, lang, itself, commands_re,
-                 chains, scheduler, main_component_id, bot_id,
+                 commands, chains, scheduler, main_component_id, bot_id,
                  shared_memory):
         # This attribute should be added with the default setattr, because is
         # needed by the custom setattr
@@ -43,6 +43,7 @@ class FrozenBot:
         self._shared_memory = shared_memory
         self._scheduler = scheduler
         self._chains = chains
+        self._commands = commands
 
         # Setup the logger
         self.logger = logbook.Logger('botogram bot')
@@ -61,9 +62,9 @@ class FrozenBot:
         args = (
             self.api, self.about, self.owner, self.hide_commands,
             self.before_help, self.after_help, self.process_backlog,
-            self.lang, self.itself, self._commands_re, self._chains,
-            self._scheduler, self._main_component_id, self._bot_id,
-            self._shared_memory,
+            self.lang, self.itself, self._commands_re, self._commands,
+            self._chains, self._scheduler, self._main_component_id,
+            self._bot_id, self._shared_memory,
         )
         return restore, args
 