7c107fa745ee617d419fb225143c0f463e7a184b
Pietro Albini <pietro@pietroalbini.io>
2016-03-29 12:02:10 +0200

    Use a smarter algorithm to filter out backlog messages
    Now the runner doesn't check every message if they're from the
    backlog at the start of the bot, but it tries to bulk check them in a
    smarter way. This increases performances a lot if you've big backlogs.

--- botogram/updates.py
+++ botogram/updates.py
@@ -49,17 +49,56 @@ class UpdatesFetcher:
         if self._backlog_processed:
             return updates, []
 
-        result = []
-        backlog = []
-        for update in updates:
-            if update.message.date < self._started_at:
-                backlog.append(update)
-                continue
+        # Now start to filter backlog from messages to process
+        # This is faster than a plain check for every item with a for, and
+        # helps a lot if you have a *really* long backlog
 
-            result.append(update)
+        if not updates:
             self._backlog_processed = True
+            return [], []
 
-        return result, backlog
+        to_check = len(updates) - 1
+        check_chunk = len(updates)
+        direction = -1  # -1 is backward, 1 is forward
+        last = 0
+
+        while True:
+            # Check if the current message to check is from the backlog
+            # Adjust the check direction accordingly
+            update = updates[to_check]
+            if update.message.date < self._started_at:
+                direction = 1
+            else:
+                direction = -1
+
+            # The next chunk to check is exactly half of the previous one
+            check_chunk = int(check_chunk / 2)
+            if check_chunk == 0:
+                check_chunk = 1
+
+            # If the last one was from the backlog, the current one is not and
+            # this is at the max precision (step of 1), then consider it as
+            # found
+            if direction == 1 and last == -1 and check_chunk == 1:
+                to_check += 1  # Return to the previous state
+                self._backlog_processed = True
+                break
+            last = direction
+
+            # Set the next update to check
+            to_check += direction * check_chunk
+
+            # If the next update to check is outside the bounds of the list
+            # just break the loop (and say the backlog is processed if no
+            # updates are from it)
+            if to_check < 0:
+                self._backlog_processed = True
+                break
+            elif to_check >= len(updates):
+                break
+
+        # The first is the updates to process, the second the backlog
+        return updates[to_check:], updates[:to_check]
 
     @property
     def backlog_processed(self):
