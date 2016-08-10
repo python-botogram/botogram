84293b77da73379bc9cf007cb383bd3b9bfc7ab6
Pietro Albini <pietro@pietroalbini.io>
2016-08-10 12:27:27 +0200

    This is botogram 0.3.4
    This is a bugfix release, which fixes the following things:
    
    * Fix inability to send messages to supergroups by username, using the
      deprecated `botogram.Bot.send()` method (GH-74)

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2015-2016 Pietro Albini"
 author = "Pietro Albini"
 
 version = "0.3"
-release = "0.3.3"
+release = "0.3.4"
 
 language = None
 
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ import setuptools
 
 setuptools.setup(
     name = "botogram",
-    version = "0.3.3",
+    version = "0.3.4",
     url = "https://botogram.pietroalbini.io",
 
     license = "MIT",
