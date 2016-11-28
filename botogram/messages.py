1bda173b77c86069edb9d91d3452b80e24ce7c0f
Marco Aceti <marcobuster@hotmail.it>
2016-11-28 19:11:42 +0100

    Added process_channel_post and process_edited_channel_post

--- botogram/messages.py
+++ botogram/messages.py
@@ -37,3 +37,33 @@ def process_edited_message(bot, chains, update):
 
     bot.logger.debug("No hook actually processed the #%s update." %
                      update.update_id)
+
+def process_channel_post(bot, chains, update):
+    """Process a channel post"""
+    for hook in chains["channel_post"]:
+        bot.logger.debug("Processing channel_post in update #%s with the "
+                         "hook %s..." % (update.update_id, hook.name))
+
+        result = hook.call(bot, update)
+        if result is True:
+            bot.logger.debug("Update %s was just processed by the %s hook." %
+                             (update.update_id, hook.name))
+            return
+
+    bot.logger.debug("No hook actually processed the #%s update." %
+                     update.update_id)
+    
+def process_edited_channel_post(bot, chains, update):
+    """Process an edited channel post"""
+    for hook in chains["edited_channel_post"]:
+        bot.logger.debug("Processing edited_channel_post in update #%s with the "
+                         "hook %s..." % (update.update_id, hook.name))
+
+        result = hook.call(bot, update)
+        if result is True:
+            bot.logger.debug("Update %s was just processed by the %s hook." %
+                             (update.update_id, hook.name))
+            return
+
+    bot.logger.debug("No hook actually processed the #%s update." %
+                     update.update_id)
