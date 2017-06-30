954a119ff9326d2c4c81c99e4c47ab3d760cb12d
Pietro Albini <pietro@pietroalbini.org>
2017-06-30 12:54:04 +0200

    Fix lint

--- botogram/messages.py
+++ botogram/messages.py
@@ -18,6 +18,7 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #   DEALINGS IN THE SOFTWARE.
 
+
 def process_message(bot, chains, update):
     """Process a message sent to the bot"""
     for hook in chains["messages"]:
