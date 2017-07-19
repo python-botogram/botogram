8ace9e2faacb70bc3278f2f5eef74652a15e744e
Pietro Albini <pietro@pietroalbini.org>
2017-07-19 10:25:16 +0200

    Update links to the new website

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ details are managed by botogram, so you can just focus on your bot.
 Want to get started? `Go to the documentation`_
 
 .. _Telegram bots: https://core.telegram.org/bots
-.. _Go to the documentation: https://botogram.pietroalbini.io/docs
+.. _Go to the documentation: https://botogram.pietroalbini.org/docs
 '''
 
 import setuptools
@@ -38,7 +38,7 @@ import setuptools
 setuptools.setup(
     name = "botogram",
     version = "0.4.dev0",
-    url = "https://botogram.pietroalbini.io",
+    url = "https://botogram.pietroalbini.org",
 
     license = "MIT",
 
