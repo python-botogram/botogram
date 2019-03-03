e5fbaf896e9e503741a59e6b272351ed073d97d3
Marco Aceti <personale@marcoaceti.it>
2019-03-03 15:59:52 +0100

    PEP-8

--- botogram/objects/mixins.py
+++ botogram/objects/mixins.py
@@ -189,8 +189,9 @@ class ChatMixin:
 
     @_require_api
     def send_video(self, path=None, file_id=None, url=None,
-                   duration=None, caption=None, streaming=True, reply_to=None, extra=None,
-                   attach=None, notify=True, *, syntax=None):
+                   duration=None, caption=None, streaming=True,
+                   reply_to=None, extra=None, attach=None,
+                   notify=True, *, syntax=None):
         """Send a video"""
         args = self._get_call_args(reply_to, extra, attach, notify)
         args["supports_streaming"] = streaming
