837f2404e4f9735595d13341c397589980a253f0
Marco Aceti <mail@marcoaceti.it>
2019-08-07 13:00:19 +0200

    New inline worker assignment tests

--- tests/test_inline.py
+++ tests/test_inline.py
@@ -18,9 +18,6 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #   DEALINGS IN THE SOFTWARE.
 
-import pytest
-
-import botogram.objects.messages
 from botogram.components import Component
 from botogram.objects.updates import Update
 from botogram.hooks import InlineHook
@@ -30,7 +27,7 @@ from botogram.runner.jobs import _inline_assign_worker
 def inline_default(bot):
     def inline_test(inline):
         for i in range(50):
-            yield inline.article("test tilte",
+            yield inline.article("test title",
                                  "test message")
     inline_func = bot.inline()(inline_test)
     return {'inline': [InlineHook(inline_func,
@@ -39,28 +36,53 @@ def inline_default(bot):
                                    'paginate': 10, 'timer': 60})]}
 
 
-def test_assignment_worker():
-    data = {'update_id': 1,
-            'inline_query': {'id': '123',
-                             'from': {'id': 321,
-                                      'is_bot': False,
-                                      'first_name': 'test_name'},
-                             'query': '',
-                             'offset': ''}}
-    update = Update(data)
-    worker_id = _inline_assign_worker(update, 5)
-    assert worker_id == 4
+def test_worker_assignment():
+    update_1 = Update({
+        'update_id': 1,
+        'inline_query': {
+            'id': '123456789012345',
+            'from': {
+                'id': '12345678',
+                'is_bot': False,
+                'first_name': 'test_name',
+            },
+            'query': '',
+            'offset': '',
+        }
+    })
+    assert _inline_assign_worker(update_1, 5) == 1
+
+    update_2 = Update({
+        'update_id': 2,
+        'inline_query': {
+            'id': '0987654321012345',
+            'from': {
+                'id': '87654321',
+                'is_bot': False,
+                'first_name': 'test_name',
+            },
+            'query': 'Test query with some unicode 🅱️🤯🥶',
+            'offset': '10',
+        }
+    })
+    assert _inline_assign_worker(update_2, 3) == 2
 
 
 def test_inline_hook(bot, mock_req):
     chains = inline_default(bot)
-    update = Update({'update_id': 1,
-                     'inline_query': {'id': '123',
-                                      'from': {'id': 321,
-                                               'is_bot': False,
-                                               'first_name': 'test_name'},
-                                      'query': '',
-                                      'offset': ''}})
+    update = Update({
+        'update_id': 1,
+        'inline_query': {
+            'id': '123',
+            'from': {
+                'id': 321,
+                'is_bot': False,
+                'first_name': 'test_name'
+            },
+            'query': '',
+            'offset': ''
+        }
+    })
     mock_req({
         'answerInlineQuery': {'ok': True, 'result': True}
     })
@@ -68,18 +90,26 @@ def test_inline_hook(bot, mock_req):
 
 
 def test_paginate(bot, mock_req):
-    pass #todo
-    """
+    return "¿?"
     chains = inline_default(bot)
-    update = Update({'update_id': 1,
-                     'inline_query': {'id': '123',
-                                      'from': {'id': 321,
-                                               'is_bot': False,
-                                               'first_name': 'test_name'},
-                                      'query': '',
-                                      'offset': ''}})
+    update = Update({
+        'update_id': 1,
+        'inline_query': {
+            'id': '123',
+            'from': {
+                'id': 321,
+                'is_bot': False,
+                'first_name': 'test_name'
+            },
+            'query': 'myquery',
+            'offset': ''
+        }
+    })
     mock_req({
-        'answerInlineQuery': {'ok': True, 'result': True}
+        'answerInlineQuery': {
+            'ok': True,
+            'result': True
+        }
     })
     bot._update_processors['inline_query'](bot, chains, update)
-    assert False"""
+    assert False
