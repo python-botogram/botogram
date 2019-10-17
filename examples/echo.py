077249f5f5d47a77e108946a3f2a8b8b75f9ee56
Mattia <idoppioclick@gmail.com>
2019-10-17 16:15:25 +0200

    Added two basic code examples

---
+++ examples/echo.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015-2019 The Botogram Authors (see AUTHORS)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+
+import botogram
+bot = botogram.create("256918030:AAEUIEgYrgBBvTF3iwK4tP6DVJf659dPMlc")
+
+
+def disable_command_not_found_message(chat, message):
+    """ This function will disable the "command not found" message, so that
+        if the user sends a message starting with "/" it will be echoed without
+        the bot sending the "command not found"
+    """
+    pass
+
+
+@bot.before_processing
+def message_reading(chat, message):
+    chat.send(message.text)
+
+
+# Enable the custom "command not found" function
+bot._components[0]._Component__no_commands = []
+bot._main_component._add_no_commands_hook(disable_command_not_found_message)
+
+
+if __name__ == "__main__":
+    bot.run()
